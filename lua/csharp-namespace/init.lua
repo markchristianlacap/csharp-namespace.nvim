8984ad86e2d7ed5df6e709378d6463eabf5e787a
Mark Christian Lacap <markchristianlacap@gmail.com>
2026-05-23 08:50:45 +0800

    chore: debugging

--- lua/csharp-namespace/init.lua
+++ lua/csharp-namespace/init.lua
@@ -7,20 +7,19 @@ return {
 			require("cmp").register_source("csharp-namespace", require("csharp-namespace.completion.nvim-cmp"))
 		end
 		if opts.completefunc then
+			_G.csharp_namespace_completefunc = function(findstart, base)
+				return require("csharp-namespace.completion.completefunc").completefunc(findstart, base)
+			end
 			local group = vim.api.nvim_create_augroup("csharp_namespace_completefunc", { clear = true })
 			vim.api.nvim_create_autocmd("FileType", {
 				group = group,
 				pattern = "cs",
 				callback = function()
-					vim.bo.completefunc = function(findstart, base)
-						return require("csharp-namespace.completion.completefunc").completefunc(findstart, base)
-					end
+					vim.bo.completefunc = "v:lua.csharp_namespace_completefunc"
 				end,
 			})
 			if vim.bo.filetype == "cs" then
-				vim.bo.completefunc = function(findstart, base)
-					return require("csharp-namespace.completion.completefunc").completefunc(findstart, base)
-				end
+				vim.bo.completefunc = "v:lua.csharp_namespace_completefunc"
 			end
 		end
 	end,
